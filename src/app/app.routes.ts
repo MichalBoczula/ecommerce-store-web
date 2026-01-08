54603e68e97175ec609a2c683b4f0d28ccc927b8
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-01-08 07:48:14 +0100

    Add mobile phones page and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,12 @@ export const routes: Routes = [
             import('./features/categories/presentation/categories.routes')
                 .then(m => m.CATEGORIES_ROUTES),
     },
+    {
+        path: 'mobile-phones',
+        loadChildren: () =>
+            import('./features/mobile-phones/presentation/mobile-phones.routes')
+                .then(m => m.MOBILE_PHONES_ROUTES),
+    },
 
     { path: '**', redirectTo: 'categories' },
 ];
