6661fb6c56f9c3c67e0fd29774e50d4eb76dc2c2
Michal Boczula <michal.boczula0101@gmail.com>
2026-01-28 21:09:06 +0100

    Remove primeNg and add swiper

--- src/app/features/mobile-phones/presentation/mobile-phone.details/mobile-phone.details.ts
+++ src/app/features/mobile-phones/presentation/mobile-phone.details/mobile-phone.details.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-mobile-phone-details',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './mobile-phone.details.html',
   styleUrl: './mobile-phone.details.scss',
 })
 export class MobilePhoneDetails {
   cards = [1, 2, 3];
+
+  readonly images = [
+    'https://material.angular.dev/assets/img/examples/shiba2.jpg',
+    'https://material.angular.dev/assets/img/examples/shiba1.jpg',
+    'https://material.angular.dev/assets/img/examples/shiba2.jpg',
+  ];
 }
