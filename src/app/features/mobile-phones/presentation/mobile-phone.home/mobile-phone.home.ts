40fcd06a0af517e46fa8e7212f740afb495b7283
Michal Boczula <michal.boczula0101@gmail.com>
2026-02-08 20:45:26 +0100

    Added home comp and css

--- src/app/features/mobile-phones/presentation/mobile-phone.home/mobile-phone.home.ts
+++ src/app/features/mobile-phones/presentation/mobile-phone.home/mobile-phone.home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-phone-home',
-  imports: [RouterLink],
+  imports: [RouterLink, MatIconModule, MatButtonModule],
   templateUrl: './mobile-phone.home.html',
   styleUrl: './mobile-phone.home.scss',
 })
