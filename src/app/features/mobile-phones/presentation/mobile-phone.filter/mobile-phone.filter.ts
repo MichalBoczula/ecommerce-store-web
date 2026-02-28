811082593d5d3dc489e1cb57fc381476bbe7ee0b
Michal Boczula <michal.boczula0101@gmail.com>
2026-02-28 20:44:24 +0100

    Added form

--- src/app/features/mobile-phones/presentation/mobile-phone.filter/mobile-phone.filter.ts
+++ src/app/features/mobile-phones/presentation/mobile-phone.filter/mobile-phone.filter.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-mobile-phone-filter',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatSliderModule],
   templateUrl: './mobile-phone.filter.html',
   styleUrl: './mobile-phone.filter.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MobilePhoneFilter {
 
