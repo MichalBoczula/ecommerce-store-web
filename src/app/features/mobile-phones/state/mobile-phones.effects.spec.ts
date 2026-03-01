372aec75c2db99ecb995bb00fb02e37a558e8638
Michal Boczula <michal.boczula0101@gmail.com>
2026-03-01 17:10:37 +0100

    Refactored and added task to-do

--- src/app/features/mobile-phones/state/mobile-phones.effects.spec.ts
+++ src/app/features/mobile-phones/state/mobile-phones.effects.spec.ts
@@ -1,13 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Observable, of, ReplaySubject, throwError } from 'rxjs';
-
 import { MobilePhonesEffects } from './mobile-phones-effects';
 import * as Actions from './mobile-phones.actions';
 import { MobilePhonesRepository } from '../domain/interfaces/mobile-phones-repository.port';
 import { MobilePhone } from '../domain/model/mobile-phone';
-import { MobilePhoneDetails } from '../domain/model/mobile-phone-details';
-import { TopMobilePhone } from '../domain/model/top-mobile-phone';
 
 describe('MobilePhonesEffects', () => {
     let actions$: ReplaySubject<any>;
@@ -57,60 +54,6 @@ describe('MobilePhonesEffects', () => {
             expect(action).toEqual(
                 Actions.loadMobilePhonesFailure({ error: 'Error: api failed' })
             );
-            done();
-        });
-    });
-
-    it('loadById$ should return success', (done) => {
-        const item = { id: 'abc' } as MobilePhoneDetails;
-        repo.getById.and.returnValue(of(item));
-
-        actions$.next(Actions.loadMobilePhoneById({ id: 'abc' }));
-
-        effects.loadById$.subscribe(action => {
-            expect(repo.getById).toHaveBeenCalledWith('abc');
-            expect(action).toEqual(Actions.loadMobilePhoneByIdSuccess({ item }));
-            done();
-        });
-    });
-
-    it('create$ should call create and return success', (done) => {
-        const created = { id: 'created-1' } as MobilePhoneDetails;
-        repo.create.and.returnValue(of(created));
-
-        actions$.next(Actions.createMobilePhone());
-
-        effects.create$.subscribe(action => {
-            expect(repo.create).toHaveBeenCalled();
-            expect(action).toEqual(Actions.createMobilePhoneSuccess({ item: created }));
-            done();
-        });
-    });
-
-    it('loadTop$ should return success', (done) => {
-        const items = [{ id: 'top-1' }] as TopMobilePhone[];
-        repo.getTopMobilePhones.and.returnValue(of(items));
-
-        actions$.next(Actions.loadTopMobilePhone());
-
-        effects.loadTop$.subscribe(action => {
-            expect(repo.getTopMobilePhones).toHaveBeenCalled();
-            expect(action).toEqual(Actions.loadTopMobilePhoneSuccess({ items }));
-            done();
-        });
-    });
-
-    it('loadByFilter$ should return success', (done) => {
-        const filter = { brand: 'Samsung' } as any;
-        const items = [{ id: '1' }] as MobilePhone[];
-
-        repo.getFilteredMobilePhones.and.returnValue(of(items));
-
-        actions$.next(Actions.loadMobilePhoneByFilter({ filter }));
-
-        effects.loadByFilter$.subscribe(action => {
-            expect(repo.getFilteredMobilePhones).toHaveBeenCalledWith(filter);
-            expect(action).toEqual(Actions.loadMobilePhoneByFilterSuccess({ items }));
             done();
         });
     });
--- src/app/features/mobile-phones/state/mobile-phones.feature.spec.ts
+++ src/app/features/mobile-phones/state/mobile-phones.feature.spec.ts
@@ -4,8 +4,6 @@ import {
     MobilePhonesState,
 } from './mobile-phones.feature';
 import { MobilePhone } from '../domain/model/mobile-phone';
-import { MobilePhoneDetails } from '../domain/model/mobile-phone-details';
-import { TopMobilePhone } from '../domain/model/top-mobile-phone';
 
 describe('mobilePhonesFeature reducer', () => {
     const reducer = mobilePhonesFeature.reducer;
@@ -58,57 +56,5 @@ describe('mobilePhonesFeature reducer', () => {
 
         expect(result.status).toBe('error');
         expect(result.error).toBe('boom');
-    });
-
-    it('should set selectedItem on loadMobilePhoneByIdSuccess', () => {
-        const item = { id: '123' } as MobilePhoneDetails;
-
-        const result = reducer(
-            initialState,
-            Actions.loadMobilePhoneByIdSuccess({ item })
-        );
-
-        expect(result.status).toBe('loaded');
-        expect(result.selectedItem).toEqual(item);
-    });
-
-    it('should set topMobilePhones on loadTopMobilePhoneSuccess', () => {
-        const items = [{ id: 'top-1' }] as TopMobilePhone[];
-
-        const result = reducer(
-            initialState,
-            Actions.loadTopMobilePhoneSuccess({ items })
-        );
-
-        expect(result.status).toBe('loaded');
-        expect(result.topMobilePhones).toEqual(items);
-    });
-
-    it('should replace items on loadMobilePhoneByFilterSuccess', () => {
-        const previousState: MobilePhonesState = {
-            ...initialState,
-            items: [{ id: 'old' } as MobilePhone],
-        };
-
-        const items = [{ id: 'new-1' }] as MobilePhone[];
-
-        const result = reducer(
-            previousState,
-            Actions.loadMobilePhoneByFilterSuccess({ items })
-        );
-
-        expect(result.status).toBe('loaded');
-        expect(result.items).toEqual(items);
-    });
-
-    it('should set loaded on createMobilePhoneSuccess', () => {
-        const item = { id: 'created-1' } as MobilePhoneDetails;
-
-        const result = reducer(
-            initialState,
-            Actions.createMobilePhoneSuccess({ item })
-        );
-
-        expect(result.status).toBe('loaded');
     });
 });
