1e4055cc09ac1b973fc7760d09c19d68f65fb33a
Michal Boczula <michal.boczula0101@gmail.com>
2026-08-09 15:53:56 +0200

    Added ShoppingCartComp

--- src/app/features/mobile-phones/presentation/mobile-phone.container/mobile-phone.container.ts
+++ src/app/features/mobile-phones/presentation/mobile-phone.container/mobile-phone.container.ts
@@ -1,4 +1,4 @@
-import { Component, computed } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
@@ -6,7 +6,8 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MobilePhoneFilter } from "../mobile-phone.filter/mobile-phone.filter";
-import { RouterOutlet } from "@angular/router";
+import { Router, RouterOutlet } from "@angular/router";
+import { MobilePhonesFacade } from '../../application/mobile-phones.facade';
 
 @Component({
   selector: 'app-mobile-phone-container',
@@ -27,8 +28,13 @@ import { RouterOutlet } from "@angular/router";
 export class MobilePhoneContainer {
   readonly sidenavMode = computed<'over'>(() => 'over');
   readonly sidenavOpened = computed(() => false);
+  private readonly router = inject(Router);
 
   onNavClick(sidenav: { close: () => void }) {
     sidenav.close();
+  }
+
+  onCartClick(): void {
+    this.router.navigate(['/cart']);
   }
 }
